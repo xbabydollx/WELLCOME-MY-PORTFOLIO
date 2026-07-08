28c35e872b5ac157cb8c2942abd7521abb3ce04f
xbabydollx <115411130+xbabydollx@users.noreply.github.com>
2026-07-09 06:17:03 +0700

    fix: adjust basepath for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Wajib untuk GitHub Pages agar jadi HTML statis
+  output: 'export', // Wajib untuk GitHub Pages
+  basePath: '/WELLCOME-MY-PORTFOLIO', // WAJIB! Agar GitHub mengenali aset di dalam sub-folder ini
   images: {
-    unoptimized: true, // Matikan optimasi gambar bawaan server Next.js
+    unoptimized: true,
   },
 };
 
